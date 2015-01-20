8c260404da20a61df8922dcd5f50124bce53ad9a
jmdx <jdmaddux@gmail.com>
2015-01-19 22:26:39 -0800

    Add score display.

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,15 @@
 (function() {
   "use strict";
+  var currentScore = 0,
+      scoreDiv = $("#scoreArea");
+  function incrementScore() {
+    scoreDiv.text(++currentScore);
+  }
   var player = new dotNinjas.PlayerEntity(dotNinjas.config.cols - 1,
                                           dotNinjas.config.rows - 1,
                                           "green"),
       enemy = new dotNinjas.EnemyEntity(0, 0, player),
-      coin = new dotNinjas.CoinEntity(5, 0, player, function() {}, "yellow"),
+      coin = new dotNinjas.CoinEntity(5, 0, player, incrementScore, "yellow"),
       updateableEntities = [player, enemy, coin];
   setInterval(function() {
     _(updateableEntities).forEach(function(entity) {
